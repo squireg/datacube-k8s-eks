6c5be208e210966a03580528d03715bc92b68df9
Squire, Geoffrey (S&A, Black Mountain) <geoffrey.squire@csiro.au>
2023-08-14 15:08:06 +1000

    Null add-on config is null!

--- odc_eks/addons.tf
+++ odc_eks/addons.tf
@@ -4,7 +4,7 @@ resource "aws_eks_addon" "vpc_cni" {
     cluster_name  = module.eks.cluster_id
     addon_name    = "vpc-cni"
     addon_version = var.addon_vpccni_version
-    configuration_values = jsonencode(var.addon_vpccni_config)
+    configuration_values = var.addon_vpccni_config == null ? null : jsonencode(var.addon_vpccni_config)
 
     # This will change to resolve_conflicts_{create,update} using separate vars
     # when we upgrade to aws provider v5.
@@ -17,7 +17,7 @@ resource "aws_eks_addon" "kube-proxy" {
     cluster_name  = module.eks.cluster_id
     addon_name    = "kube-proxy"
     addon_version = var.addon_kubeproxy_version
-    configuration_values = jsonencode(var.addon_kubeproxy_config)
+    configuration_values = var.addon_kubeproxy_config == null ? null : jsonencode(var.addon_kubeproxy_config)
 
     # This will change to resolve_conflicts_{create,update} using separate vars
     # when we upgrade to aws provider v5.
